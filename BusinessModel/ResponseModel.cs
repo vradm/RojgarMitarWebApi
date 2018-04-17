b0d044f5af0a972a9a9d869747c436bab668d653
vradm <37693095+vradm@users.noreply.github.com>
2018-04-17 19:01:07 +0530

    Initial Commit

---
+++ BusinessModel/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RojgarMitraWebApi.BusinessModel
+{
+    public class ResponseModel
+    {
+        public long? id { get; set; }
+        public string message { get; set; }
+        public object data { get; set; }
+        public bool status { get; set; } = false;
+        public string redirectUrl { get; set; }
+    }
+}
